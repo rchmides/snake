3797499589e5ea6dccba12adb5eb88593263bd7b
r4mides <artem.fomin633@gmail.com>
2019-02-13 19:33:16 +0300

    1

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -8,5 +8,26 @@ namespace Snake
 {
     class Point
     {
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point()
+        {
+        }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
     }
 }
