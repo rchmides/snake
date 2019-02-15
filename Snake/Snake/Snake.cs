af54a9692557762e8e5ce0164f28a2fa5a1f905b
r4mides <artem.fomin633@gmail.com>
2019-02-15 13:51:06 +0300

    7

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -10,32 +10,14 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
+            Point p1 = new Point(12, 3, '*');
             p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            HorizontalLine upperLine = new HorizontalLine(0, 70, 0, '—');
+            upperLine.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
+            HorizontalLine downLine = new HorizontalLine(0, 70, 20, '—');
+            downLine.Draw();
             Console.ReadLine();
         }
     }
